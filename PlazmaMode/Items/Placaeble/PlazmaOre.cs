3b99ff2ed5fcf53f4a953cec47586767749bf9d1
martosinc <60327729+martosinc@users.noreply.github.com>
2020-08-05 20:20:24 +0300

    Add files via upload

--- PlazmaMode/Items/Placaeble/PlazmaOre.cs
+++ PlazmaMode/Items/Placaeble/PlazmaOre.cs
@@ -1,20 +1,30 @@
-using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ID;
 
-namespace PlazmaMode.Items.Bars
+namespace PlazmaMode.Items.Placeable
 {
-	public class PlazmaOre : ModItem
-	{
-        public override void SetDefaults() {
-            item.name = "Плазматическая руда";
-            item.width = 12;
-            item.height = 12;
-            item.tooltip = "Из самых жарких мест планеты";
-            item.useTime = 20;
-            item.value = 10000;
-            item.rare = 2;
-            item.UseSound = SoundID.Item1;
-            item.autoReuse = true;
+    public class PlazmaOre : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Plazma Ore");
+            Tooltip.SetDefault("This is a modded ore.");
         }
-	}
+
+        public override void SetDefaults()
+        {
+            item.width = 12; // Hitbox Width
+            item.height = 12; // Hitbox Height
+            item.useTime = 20; // Speed before reuse
+            item.useAnimation = 20; // Animation Speed
+            item.useStyle = 1; // 1 = Broadsword 
+            item.value = 50; // 10 | 00 | 00 | 00 : Platinum | Gold | Silver | Bronze
+            item.rare = 2; // Item Tier
+            item.UseSound = SoundID.Item1; // Sound effect of item on use 
+            item.autoReuse = true; // Do you want to torture people with clicking? Set to false
+            item.consumable = true; // Will consume the item when placed.
+            item.createTile = mod.TileType("PlazmaOreTile");
+            item.maxStack = 999; // The maximum number you can have of this item.
+        }
+    }
 }
